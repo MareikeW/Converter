39b3f4f6a83f5d248058114b018b7684b744f2af
Mareike <49586574+MareikeW@users.noreply.github.com>
2019-05-10 09:34:24 +0200

    Update convertStintoKg.java

--- convertStintoKg.java
+++ convertStintoKg.java
@@ -1,9 +1,16 @@
-public class ConverStintoKg{
-    public static void main(String[] args){
-        System.out.println(convertStintoKg(8));
-    }
-    public static double convertStintoKg(int St){
-        double Kg = St * 6.35;
-        return Kg;
-    }
+import java.util.Scanner;
+//in this program you can enter weight in stone and convert it into kg.
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner number = new Scanner(System.in);
+		System.out.println("Enter the number of stones you want to convert into kg: ");//displays
+		int st = number.nextInt();//reads number from the keyboard
+		System.out.println(convertStintoKg(st));//displays converted number
+		
+	}
+	public static double convertStintoKg(int st){
+	    double kg = st * 6.35;//conversion
+	    return kg;
+	}
 }
