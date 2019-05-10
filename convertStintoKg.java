ac15452b37516877523330f8b6181fc6971e7817
Mareike <49586574+MareikeW@users.noreply.github.com>
2019-05-10 09:17:57 +0200

    Create convertStintoKg.java

---
+++ convertStintoKg.java
@@ -0,0 +1,9 @@
+public class ConverStintoKg{
+    public static void main(String[] args){
+        System.out.println(convertStintoKg(8));
+    }
+    public static double convertStintoKg(int St){
+        double Kg = St * 6.35;
+        return Kg;
+    }
+}
